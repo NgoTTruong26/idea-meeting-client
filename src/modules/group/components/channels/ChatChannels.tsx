31a23efc09957c3d9241be1250e2e49b22b845df
unknown <tiennhannaruto@gmail.com>
2023-12-11 23:36:38 +0700

    update: UI direct group messages

--- src/modules/group/components/channels/ChatChannels.tsx
+++ src/modules/group/components/channels/ChatChannels.tsx
@@ -1,7 +1,14 @@
+import { MdPersonAddAlt } from "react-icons/md"
+
 export default function ChatChannels() {
   return (
-    <div>
-      <div>Chat channels</div>
+    <div className="[&>div]:px-2">
+      <div>
+        <span>Chat channels</span>
+        <span>
+          <MdPersonAddAlt size={18} />
+        </span>
+      </div>
     </div>
   )
 }
--- src/modules/group/pages/DirectGroupMessages.tsx
+++ src/modules/group/pages/DirectGroupMessages.tsx
@@ -1,19 +1,125 @@
-import { Divider } from "@nextui-org/react"
+import {
+  Accordion,
+  AccordionItem,
+  Button,
+  Divider,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+} from "@nextui-org/react"
+import { useState } from "react"
+import { AiOutlineUser } from "react-icons/ai"
+import {
+  MdAdd,
+  MdKeyboardArrowDown,
+  MdOutlineClose,
+  MdPersonAddAlt,
+} from "react-icons/md"
+import { TbEdit } from "react-icons/tb"
 import { Outlet } from "react-router-dom"
 import ChatChannels from "../components/channels/ChatChannels"
 import VoiceChannels from "../components/channels/VoiceChannels"
 
 export default function DirectGroupMessages() {
+  const [showDropdown, setShowDropdown] = useState<boolean>(false)
+
+  const handleOpenChange = (isOpen: boolean) => {
+    setShowDropdown(isOpen)
+  }
+
   return (
     <div className="grid grid-cols-[22rem,1fr]">
-      <div>
-        <div>
-          <div>HiHi</div>
-          <div>User: 20</div>
-        </div>
+      <div className="h-screen px-4 pb-5 border-x-2 bg-gray-50 space-y-4 overflow-y-auto">
+        <Dropdown
+          placement="bottom-end"
+          onOpenChange={handleOpenChange}
+          className="py-2 space-y-2"
+        >
+          <DropdownTrigger>
+            <Button
+              fullWidth
+              className="h-fit py-2 mt-2"
+              color="primary"
+              variant="light"
+              endContent={
+                showDropdown ? (
+                  <MdOutlineClose size={20} />
+                ) : (
+                  <MdKeyboardArrowDown size={20} />
+                )
+              }
+            >
+              <div className="w-full flex flex-col items-start space-y-1">
+                <div className="font-semibold text-xl">HiHi</div>
+                <div className="flex items-end space-x-1 text-sm">
+                  <span>
+                    <AiOutlineUser size={22} />
+                  </span>
+                  <span>20 members</span>
+                </div>
+              </div>
+            </Button>
+          </DropdownTrigger>
+          <DropdownMenu
+            aria-label="Dynamic Actions"
+            color="primary"
+            className="text-primary"
+          >
+            <DropdownItem
+              key="add-member"
+              endContent={<MdPersonAddAlt size={18} />}
+            >
+              Add Member
+            </DropdownItem>
+            <DropdownItem key="edit-group" endContent={<TbEdit size={18} />}>
+              Edit Group Profile
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
         <Divider />
-        <ChatChannels />
-        <VoiceChannels />
+
+        <Accordion
+          variant="light"
+          selectionMode="multiple"
+          className="pt-2 px-0 space-y-4"
+          showDivider={false}
+        >
+          <AccordionItem
+            key="chat-channels"
+            aria-label="Chat Channels"
+            title="Chat Channels"
+            classNames={{
+              trigger: "py-0 pr-2",
+              content: "space-y-2",
+            }}
+          >
+            <ChatChannels />
+            <div className="flex gap-4 justify-between text-sm text-primary px-2">
+              <span>Add Chat Channel</span>
+              <span className="w-4">
+                <MdAdd size={14} />
+              </span>
+            </div>
+          </AccordionItem>
+          <AccordionItem
+            key="voice-channels"
+            aria-label="Voice Channels"
+            title="Voice Channels"
+            classNames={{
+              trigger: "py-0 pr-2",
+              content: "space-y-2",
+            }}
+          >
+            <VoiceChannels />
+            <div className="flex gap-4 justify-between text-sm text-primary px-2">
+              <span>Add Voice Channel</span>
+              <span className="w-4">
+                <MdAdd size={14} />
+              </span>
+            </div>
+          </AccordionItem>
+        </Accordion>
       </div>
       <Outlet />
     </div>
