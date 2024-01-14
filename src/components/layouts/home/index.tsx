bf6e52c5c8bae200952aa298555e82ffc487daec
unknown <tiennhannaruto@gmail.com>
2024-01-14 17:44:07 +0700

    fix: overflow of home, home group

--- src/components/layouts/home/index.tsx
+++ src/components/layouts/home/index.tsx
@@ -4,7 +4,7 @@ import Sidebar from "./Sidebar"
 
 export default function HomeLayout() {
   return (
-    <div className="h-screen grid grid-cols-[72px,1fr]">
+    <div className="h-screen grid grid-cols-[72px,1fr] overflow-y-hidden">
       <Sidebar />
       <Outlet />
       <CallOverlay />
--- src/modules/group/pages/HomeGroupChannel.tsx
+++ src/modules/group/pages/HomeGroupChannel.tsx
@@ -31,7 +31,7 @@ export default function HomeGroupChannel() {
     <div className="relative flex justify-center items-center">
       <div className="absolute flex justify-center items-center max-w-3xl w-full h-full max-h-[750px] bg-[url('/images/sign-in-bg.jpg')] bg-cover bg-center"></div>
       <div className="flex justify-center items-center w-full backdrop-blur-xl h-full">
-        <div className="flex flex-col items-center max-w-xl w-full space-y-6 p-8 rounded-3xl">
+        <div className="flex flex-col items-center max-w-xl w-full space-y-6 p-8 rounded-3xl overflow-y-auto max-h-screen">
           <div className="text-3xl font-bold">
             Welcome to {groupProfile.name}
           </div>
